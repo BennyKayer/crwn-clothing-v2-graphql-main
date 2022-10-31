7d935c126d64684c7b585a7a988c32b70d40382e
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-10-31 14:39:36 +0100

    #25.225 Updating Components To Consume GraphQL Values

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -24,10 +24,19 @@ export const CategoriesProvider = ({ children }) => {
     const { loading, error, data } = useQuery(COLLECTIONS);
     const [categoriesMap, setCategoriesMap] = useState({});
 
-    console.log("data", data);
-    console.log("loading", loading);
+    useEffect(() => {
+        if (data) {
+            const { collections } = data;
+            const collectionsMap = collections.reduce((acc, collection) => {
+                const { title, items } = collection;
+                acc[title.toLowerCase()] = items;
+                return acc;
+            }, {});
+            setCategoriesMap(collectionsMap);
+        }
+    }, [data]);
 
-    const value = { categoriesMap };
+    const value = { categoriesMap, loading };
     return (
         <CategoriesContext.Provider value={value}>
             {children}
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,32 +1,42 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { useContext, useState, useEffect, Fragment } from "react";
+import { useParams } from "react-router-dom";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import { CategoriesContext } from "../../contexts/categories.context";
 
-import { CategoryContainer, Title } from './category.styles';
+import { CategoryContainer, Title } from "./category.styles";
+import Spinner from "../../components/spinner/spinner.component";
 
 const Category = () => {
-  const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+    const { category } = useParams();
+    const { categoriesMap, loading } = useContext(CategoriesContext);
+    const [products, setProducts] = useState(categoriesMap[category]);
 
-  useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    useEffect(() => {
+        setProducts(categoriesMap[category]);
+    }, [category, categoriesMap]);
 
-  return (
-    <Fragment>
-      <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            {loading ? (
+                <Spinner />
+            ) : (
+                <>
+                    <Title>{category.toUpperCase()}</Title>
+                    <CategoryContainer>
+                        {products &&
+                            products.map((product) => (
+                                <ProductCard
+                                    key={product.id}
+                                    product={product}
+                                />
+                            ))}
+                    </CategoryContainer>
+                </>
+            )}
+        </Fragment>
+    );
 };
 
 export default Category;
